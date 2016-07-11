17414aef376959476019b0a73d7013af8292b29d
Dustin Steiner <dustin.steiner@fh-hagenberg.at>
2016-07-11 11:22:06 +0200

    crash fix when service not initialized

--- app/src/main/java/at/fhhagenberg/mint/automate/accessibilityrunner/ui/ManagerListActivity.java
+++ app/src/main/java/at/fhhagenberg/mint/automate/accessibilityrunner/ui/ManagerListActivity.java
@@ -45,138 +45,141 @@ import at.fhhagenberg.mint.automate.loggingclient.javacore.kernel.KernelListener
  * A simple activity that lists all registered managers with the possibility to disable or enable them.
  */
 public class ManagerListActivity extends AppCompatActivity {
-    private RecyclerView mRecyclerView;
-    private ManagerAdapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+	private RecyclerView mRecyclerView;
+	private ManagerAdapter mAdapter;
+	private RecyclerView.LayoutManager mLayoutManager;
 
-    private TextView mDeviceIdText;
+	private TextView mDeviceIdText;
 
-    private KernelListener mKernelListener = new KernelListener() {
-        @Override
-        public void startupFinished() {
-            updateKernelStatus();
-            updateUserId();
-            mAdapter.initManagers();
-            mAdapter.setInteractivityEnabled(true);
-        }
+	private KernelListener mKernelListener = new KernelListener() {
+		@Override
+		public void startupFinished() {
+			updateKernelStatus();
+			updateUserId();
+			mAdapter.initManagers();
+			mAdapter.setInteractivityEnabled(true);
+		}
 
-        @Override
-        public void onPrepareShutdown() {
-            getSupportActionBar().setTitle("Status: shutting down");
-        }
+		@Override
+		public void onPrepareShutdown() {
+			getSupportActionBar().setTitle("Status: shutting down");
+		}
 
-        @Override
-        public void onShutdown() {
-            updateKernelStatus();
-            updateUserId();
-            mAdapter.setInteractivityEnabled(false);
-        }
-    };
+		@Override
+		public void onShutdown() {
+			updateKernelStatus();
+			updateUserId();
+			mAdapter.setInteractivityEnabled(false);
+		}
+	};
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manager_list);
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        mDeviceIdText = (TextView) findViewById(R.id.deviceIdText);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_manager_list);
+		mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+		mDeviceIdText = (TextView) findViewById(R.id.deviceIdText);
 
-        mRecyclerView.setHasFixedSize(true);
+		mRecyclerView.setHasFixedSize(true);
 
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
+		mLayoutManager = new LinearLayoutManager(this);
+		mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mAdapter = new ManagerAdapter();
-        mRecyclerView.setAdapter(mAdapter);
-    }
+		mAdapter = new ManagerAdapter();
+		mRecyclerView.setAdapter(mAdapter);
+	}
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+	@Override
+	protected void onResume() {
+		super.onResume();
 
-        if (!SetupDialogFragment.isInstalledAsAccessibilityService(this)) {
-            SetupDialogFragment dialogFragment = new SetupDialogFragment();
-            dialogFragment.show(getSupportFragmentManager(), "setup");
-        }
+		if (!SetupDialogFragment.isInstalledAsAccessibilityService(this)) {
+			SetupDialogFragment dialogFragment = new SetupDialogFragment();
+			dialogFragment.show(getSupportFragmentManager(), "setup");
+		}
 
-        if (KernelBase.isInitialized()) {
-            KernelBase.getKernel().addListener(mKernelListener);
-        }
-        updateKernelStatus();
-        updateUserId();
-        supportInvalidateOptionsMenu();
+		if (KernelBase.isInitialized()) {
+			KernelBase.getKernel().addListener(mKernelListener);
+		}
+		updateKernelStatus();
+		updateUserId();
+		supportInvalidateOptionsMenu();
 
-        if (KernelBase.isKernelUpRunning()) {
-            mAdapter.initManagers();
-        }
-    }
+		if (KernelBase.isKernelUpRunning()) {
+			mAdapter.initManagers();
+		}
+	}
 
-    @Override
-    protected void onPause() {
-        super.onPause();
+	@Override
+	protected void onPause() {
+		super.onPause();
 
-        if (KernelBase.isInitialized()) {
-            try {
-                KernelBase.getKernel().removeListener(mKernelListener);
-            } catch (Exception ex) {
-                // Ignore
-            }
-        }
-    }
+		if (KernelBase.isInitialized()) {
+			try {
+				KernelBase.getKernel().removeListener(mKernelListener);
+			} catch (Exception ex) {
+				// Ignore
+			}
+		}
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_manager_list, menu);
-        menu.findItem(R.id.action_start).setVisible(!KernelBase.isKernelUpRunning());
-        menu.findItem(R.id.action_stop).setVisible(KernelBase.isKernelUpRunning());
-        menu.findItem(R.id.action_export).setVisible(KernelBase.isKernelUpRunning() && PropertiesHelper.getProperty(this, "fileexport.enabled", Boolean.class, false));
-        return super.onCreateOptionsMenu(menu);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_manager_list, menu);
+		menu.findItem(R.id.action_start).setVisible(!KernelBase.isKernelUpRunning());
+		menu.findItem(R.id.action_stop).setVisible(KernelBase.isKernelUpRunning());
+		menu.findItem(R.id.action_export).setVisible(KernelBase.isKernelUpRunning() && PropertiesHelper.getProperty(this, "fileexport.enabled", Boolean.class, false));
+		return super.onCreateOptionsMenu(menu);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_start: {
-                setKernelDisabled(false);
-                return true;
-            }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.action_start: {
+				setKernelDisabled(false);
+				return true;
+			}
 
-            case R.id.action_stop: {
-                setKernelDisabled(true);
-                return true;
-            }
+			case R.id.action_stop: {
+				setKernelDisabled(true);
+				return true;
+			}
 
-            case R.id.action_export: {
-                new RequestFileExportIntentAction().execute();
-                return true;
-            }
+			case R.id.action_export: {
+				new RequestFileExportIntentAction().execute();
+				return true;
+			}
 
-            default: {
-                return super.onOptionsItemSelected(item);
-            }
-        }
-    }
+			default: {
+				return super.onOptionsItemSelected(item);
+			}
+		}
+	}
 
-    private void setKernelDisabled(boolean disabled) {
-        Intent intent = new Intent(AutomateAccessibilityService.ACTION_SET_KERNEL_DISABLED_STATE);
-        intent.putExtra(AutomateAccessibilityService.EXTRA_KERNEL_DISABLED_VALUE, disabled);
-        sendBroadcast(intent);
-    }
+	private void setKernelDisabled(boolean disabled) {
+		Intent intent = new Intent(AutomateAccessibilityService.ACTION_SET_KERNEL_DISABLED_STATE);
+		intent.putExtra(AutomateAccessibilityService.EXTRA_KERNEL_DISABLED_VALUE, disabled);
+		sendBroadcast(intent);
+	}
 
-    private void updateKernelStatus() {
-        if (KernelBase.isKernelUpRunning()) {
-            getSupportActionBar().setTitle("Status: running");
-        } else {
-            getSupportActionBar().setTitle("Status: stopped");
-        }
-        supportInvalidateOptionsMenu();
-    }
+	private void updateKernelStatus() {
+		if (KernelBase.isKernelUpRunning()) {
+			getSupportActionBar().setTitle("Status: running");
+		} else {
+			getSupportActionBar().setTitle("Status: stopped");
+		}
+		supportInvalidateOptionsMenu();
+	}
 
-    private void updateUserId() {
-        CredentialManager credentialManager = (CredentialManager) KernelBase.getKernel().getManager(CredentialManager.ID);
-        if (credentialManager != null) {
-            mDeviceIdText.setText("Device id: " + credentialManager.getUserId());
-        } else {
-            mDeviceIdText.setText("Device id: unknown");
-        }
-    }
+	private void updateUserId() {
+		CredentialManager credentialManager = null;
+		if (KernelBase.isKernelUpRunning()) {
+			credentialManager = (CredentialManager) KernelBase.getKernel().getManager(CredentialManager.ID);
+		}
+		if (credentialManager != null) {
+			mDeviceIdText.setText("Device id: " + credentialManager.getUserId());
+		} else {
+			mDeviceIdText.setText("Device id: unknown");
+		}
+	}
 }
